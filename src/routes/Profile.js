25e302c61b699b4adca4b809cce1bc581e3fb2e7
Nayeon_Ryu <yunayeon1208@gmail.com>
2022-06-17 14:28:36 +0900

    [feat] 공구 자세히 보기 체크박스, 스크랩-공구자세히보기 연결

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -8,43 +8,42 @@ import Myscrap from "../components/Myscrap";
 import Myjoinlist from "../components/Myjoinlist";
 import MyQnA from "../components/MyQnA";
 
-
 const Profile = ({ refreshUser, userObj, checkObj }) => {
-    const navigate = useNavigate();
-    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
-    const onLogOutClick = () => {
-        authService.signOut();
-        navigate("/");
-    };
+  const navigate = useNavigate();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
 
-    const onChange = (event) => {
-        const {
-            target: { value },
-        } = event;
-        setNewDisplayName(value);
-    };
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        if (userObj.displayName !== newDisplayName) {
-            await userObj.updateProfile({
-                displayName: newDisplayName,
-            });
-            refreshUser();
-        }
-    };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
 
-    // 모든 startlist 불러오기
-    const [lists, setLists] = useState([]);
-    useEffect(() => {
-        dbService.collection("startlist").onSnapshot((snapshot) => {
-            const listArray = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
-            setLists(listArray);
-        });
-    }, []);
-/*
+  // 모든 startlist 불러오기
+  const [startlist, setStartlist] = useState([]);
+  useEffect(() => {
+    dbService.collection("startlist").onSnapshot((snapshot) => {
+      const listArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setStartlist(listArray);
+    });
+  }, []);
+  /*
     const [scraps, setScraps] = useState([]);
     useEffect(() => {
         dbService.doc(`startlist/${detailObj.id}`).collection("scrap").onSnapshot((snapshot) => {
@@ -56,102 +55,81 @@ const Profile = ({ refreshUser, userObj, checkObj }) => {
         });
     }, []);*/
 
+  // 모든 joinlist 불러오기
+  const [joinlist, setJoinlist] = useState([]);
+  useEffect(() => {
+    dbService.collection("joinlist").onSnapshot((snapshot) => {
+      const listArray2 = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setJoinlist(listArray2);
+    });
+  }, []);
 
-    // 모든 joinlist 불러오기
-    const [lists2, setLists2] = useState([]);
-    useEffect(() => {
-        dbService.collection("joinlist").onSnapshot((snapshot) => {
-            const listArray2 = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
-            setLists2(listArray2);
-        });
-    }, []);
+  return (
+    <div className="container">
+      <form onSubmit={onSubmit} className="profileForm">
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+          autoFocus
+          className="formInput"
+        />
+        <input
+          type="submit"
+          value="Update Profile"
+          className="formBtn"
+          style={{
+            marginTop: 10,
+          }}
+        />
+      </form>
+      <h3>내가 연 공구</h3>
+      <div>
+        {startlist.map((list) => {
+          if (list.creatorId === userObj.uid)
+            return (
+              <Mylist key={list.id} listObj={list} creatorId={list.creatorId} />
+            );
+        })}
+      </div>
 
+      <h3>스크랩 목록</h3>
+      <div>
+        {startlist.map((list) => (
+          <Myscrap
+            key={list.id}
+            listObj={list}
+            isOwner={list.creatorId === userObj.uid}
+            userObj={userObj}
+          />
+        ))}
+      </div>
+      <h3>내가 참여한 공구</h3>
+      <div>
+        {joinlist.map((list) => {
+          if (list.creatorId === userObj.uid)
+            return <Myjoinlist key={list.id} listObj={list} />;
+        })}
+      </div>
 
-    return (
-        <div className="container">
-            <form onSubmit={onSubmit} className="profileForm">
-                <input
-                    onChange={onChange}
-                    type="text"
-                    placeholder="Display name"
-                    value={newDisplayName}
-                    autoFocus
-                    className="formInput"
-                />
-                <input
-                    type="submit"
-                    value="Update Profile"
-                    className="formBtn"
-                    style={{
-                        marginTop: 10,
-                    }}
-                />
-            </form>
-            <h3>
-                내가 연 공구
-            </h3>
-            <div>
-                {lists.map((list) => (
-                    
-                    <Mylist
-                        key={list.id}
-                        listObj={list}
-                        isOwner={list.creatorId === userObj.uid}
-                        creatorId={list.creatorId}
-                        
-                    />
-                ))}
-            </div>
-
-            <h3>
-                스크랩 목록
-                
-            </h3>
-            <div>
-                {lists.map((list) => (
-                    <Myscrap
-                    key={list.id}
-                        listObj={list}
-                        isOwner={list.creatorId === userObj.uid}
-                        userObj={userObj}
-                    />
-                    
-                ))}
-                               
-            </div>
-            <h3>
-                내가 참여한 공구
-            </h3>
-            <div>
-                {lists2.map((list2) => (
-                    <Myjoinlist
-                        key={list2.id}
-                        listObj={list2}
-                        isOwner={list2.creatorId === userObj.uid}
-                    />
-                ))}
-            </div>
-
-            <h3>
-                내가 문의한 공구
-            </h3>
-            {lists.map((list) => (
-                    <MyQnA
-                        key={list.id}
-                        listObj={list}
-                        isOwner={list.creatorId === userObj.uid}
-                        userObj={userObj}
-                    />
-                    
-                ))}
-            <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
-                Log Out
-            </span>
-        </div>
-    );
-}
+      <h3>내가 문의한 공구</h3>
+      {startlist.map((list) => (
+        <MyQnA
+          key={list.id}
+          listObj={list}
+          isOwner={list.creatorId === userObj.uid}
+          userObj={userObj}
+        />
+      ))}
+      <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
+        Log Out
+      </span>
+    </div>
+  );
+};
 
 export default Profile;
