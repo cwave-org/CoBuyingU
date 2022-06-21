ee652bcc0012eae7c7ec4c81042ef49aa1b8f7c9
Nayujin <00nyj@sookmyung.ac.kr>
2022-06-21 22:04:06 +0900

    fix: 총대 표시

--- src/components/Commentlist.js
+++ src/components/Commentlist.js
@@ -1,14 +1,18 @@
 import { dbService } from "../fbase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function Commentlist({
   commentObj,
   isOwner,
   detailObj,
   qnaObj,
-  isOpener,
-}) {
+}
+) {
+ 
+
+
   const onCommentDeleteClick = async (event) => {
     if (isOwner) {
       event.preventDefault();
@@ -27,22 +31,15 @@ export default function Commentlist({
     }
   };
 
-  const CommentDelete = () => {
-    dbService
-      .doc(`startlist/${detailObj.id}`)
-      .collection("QnA")
-      .doc(`${qnaObj.id}`)
-      .collection("comments")
-      .doc(`${commentObj.id}`)
-      .delete();
-  };
-
   return (
     <div>
-      <div className="detaillist_comment1">
-          {isOpener?(
-            <span className="detaillist_comment2">
+      <div className="detaillist_qna_box">
+          {isOwner?(
+            <span className="detaillist_qnacontent">
              <b>총대</b>{' '}{commentObj.text}
+             <span className="etaillist_trashbtn" onClick={onCommentDeleteClick}>
+            <FontAwesomeIcon icon={faTrash} />
+          </span>
             </span>
             
           ):(
@@ -52,13 +49,6 @@ export default function Commentlist({
         </span>
           )}
           
-        </div>
-        <div className="detaillist_comment3">
-          {isOwner&&(
-            <span onClick={onCommentDeleteClick}>
-            <FontAwesomeIcon icon={faTrash} />
-          </span>
-          )}
         </div>
       </div>
   );
